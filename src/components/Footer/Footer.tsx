7a5c0286740a804edb1623d71213c6b03db94036
Mathias Ezequiel Latronico <mathiezelat@gmail.com>
2025-10-15 17:46:54 -0300

    Add Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+export function Footer() {
+    return (
+        <footer className="footer">
+            <small className="footer-copy">
+                © 2025. Todos los derechos reservados.
+            </small>
+        </footer>
+    );
+}
