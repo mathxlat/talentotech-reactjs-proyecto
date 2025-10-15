4b62f26adfb72329bc7f7b051d197be4c01a3848
Mathias Ezequiel Latronico <mathiezelat@gmail.com>
2025-10-15 18:40:48 -0300

    Refactor App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
+import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import "./App.css";
 
 function App() {
     return (
         <BrowserRouter>
+            <Header />
             <Routes>
-               
+                <Route index element={<ItemListContainer />} />
+                <Route path="/productos" element={<ItemListContainer />} />
             </Routes>
+            <Footer />
         </BrowserRouter>
     );
 }
