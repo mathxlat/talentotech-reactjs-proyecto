c890e724fcc4f968c34bc4515b37da793c006759
Mathias Ezequiel Latronico <mathiezelat@gmail.com>
2025-10-16 17:07:54 -0300

    Refactor App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
+import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,10 @@ function App() {
             <Routes>
                 <Route index element={<ItemListContainer />} />
                 <Route path="/productos" element={<ItemListContainer />} />
+                <Route
+                    path="/productos/:id"
+                    element={<ItemDetailContainer />}
+                />
             </Routes>
             <Footer />
         </BrowserRouter>
