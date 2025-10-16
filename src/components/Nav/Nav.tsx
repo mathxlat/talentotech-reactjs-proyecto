33d03c4bc95416cfed1ef8a5f36c21ca418414d0
Mathias Ezequiel Latronico <mathiezelat@gmail.com>
2025-10-16 18:21:16 -0300

    Refactor Nav styles

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
+import { useCartContext } from "../../context/CartContext/useCartContext";
 import "./Nav.css";
 
 export function Nav() {
+    const { getTotalItems } = useCartContext();
+
     return (
         <nav className="nav">
             <ul className="nav-list">
@@ -14,6 +17,12 @@ export function Nav() {
                     <Link className="nav-link" to="/productos">
                         Productos
                     </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/carrito">🛒</Link>
+                    {getTotalItems() > 0 && (
+                        <span className="in-cart">{getTotalItems()}</span>
+                    )}
                 </li>
             </ul>
         </nav>
