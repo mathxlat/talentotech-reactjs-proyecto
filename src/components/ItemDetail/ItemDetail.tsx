60b7ccc06f94ff777a5fb0b1e9cd8471d69a94af
Mathias Ezequiel Latronico <mathiezelat@gmail.com>
2025-10-16 18:20:50 -0300

    Refactor ItemDetail component

--- src/components/ItemDetail/ItemDetail.tsx
+++ src/components/ItemDetail/ItemDetail.tsx
@@ -1,3 +1,4 @@
+import { useCartContext } from "../../context/CartContext/useCartContext";
 import type { Product } from "../../types/product";
 import { Item } from "../Item/Item";
 import "./ItemDetail.css";
@@ -7,9 +8,17 @@ interface ItemDetailProps {
 }
 
 export function ItemDetail({ detail }: ItemDetailProps) {
+    const { addItem } = useCartContext();
+
+    const handleAddItem = () => {
+        addItem(detail);
+    };
+
     return (
         <Item {...detail}>
-            <button className="item-detail-button">Agregar al carrito</button>
+            <button onClick={handleAddItem} className="item-detail-button">
+                Agregar al carrito
+            </button>
         </Item>
     );
 }
