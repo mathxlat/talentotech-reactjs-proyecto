9441eb5f571692a7458a031cc57edea4aefab371
Mathias Ezequiel Latronico <mathiezelat@gmail.com>
2025-10-16 18:19:12 -0300

    Refactor App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,23 @@ import { Footer } from "./components/Footer/Footer";
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer";
 import "./App.css";
+import { CartProvider } from "./context/CartContext/CartProvider";
 
 function App() {
     return (
         <BrowserRouter>
-            <Header />
-            <Routes>
-                <Route index element={<ItemListContainer />} />
-                <Route path="/productos" element={<ItemListContainer />} />
-                <Route
-                    path="/productos/:id"
-                    element={<ItemDetailContainer />}
-                />
-            </Routes>
-            <Footer />
+            <CartProvider>
+                <Header />
+                <Routes>
+                    <Route index element={<ItemListContainer />} />
+                    <Route path="/productos" element={<ItemListContainer />} />
+                    <Route
+                        path="/productos/:id"
+                        element={<ItemDetailContainer />}
+                    />
+                </Routes>
+                <Footer />
+            </CartProvider>
         </BrowserRouter>
     );
 }
