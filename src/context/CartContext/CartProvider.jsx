ffea7c42c64ad4eff43f20c1d8cfc323a6c3022e
Mathias Ezequiel Latronico <mathiezelat@gmail.com>
2025-11-05 15:00:33 -0300

    Refactor CartProvider context

--- src/context/CartContext/CartProvider.jsx
+++ src/context/CartContext/CartProvider.jsx
@@ -11,12 +11,27 @@ export const CartProvider = ({ children }) => {
 
     const addItem = (item) => {
         if (exists(item.id)) {
-            alert("El producto ya existe en el carrito");
-            return;
+            const updatedCart = cart.map((product) => {
+                if (product.id === item.id) {
+                    return {
+                        ...product,
+                        quantity: product.quantity + item.quantity,
+                    };
+                } else {
+                    return product;
+                }
+            });
+            setCart(updatedCart);
+        } else {
+            setCart([...cart, item]);
         }
+        alert(`${item.name} agregado al carrito`);
+    };
 
-        setCart([...cart, item]);
-        alert(`${item.name} agregado`);
+    const deleteItem = (id) => {
+        const filteredCart = cart.filter((product) => product.id !== id);
+        setCart(filteredCart);
+        alert("Producto eliminado del productot");
     };
 
     const clearCart = () => {
@@ -24,10 +39,39 @@ export const CartProvider = ({ children }) => {
     };
 
     const getTotalItems = () => {
-        return cart.length;
+        const totalItems = cart.reduce(
+            (prev, product) => prev + product.quantity,
+            0
+        );
+        return totalItems;
     };
 
-    const values = { cart, addItem, clearCart, getTotalItems };
+    const getTotal = () => {
+        const total = cart.reduce(
+            (prev, product) => prev + product.price * product.quantity,
+            0
+        );
+        return total;
+    };
+
+    const checkout = () => {
+        const ok = confirm("¿Seguro que querés finalizar la compra?");
+
+        if (ok) {
+            alert("¡Compra realizada con éxito!");
+            clearCart();
+        }
+    };
+
+    const values = {
+        cart,
+        addItem,
+        deleteItem,
+        clearCart,
+        getTotalItems,
+        getTotal,
+        checkout,
+    };
 
     return (
         <CartContext.Provider value={values}>{children}</CartContext.Provider>
