07f5b0d92cb997c95b527419904dd55b4e178108
Mathias Ezequiel Latronico <mathiezelat@gmail.com>
2025-11-19 08:22:59 -0300

    Refactor CartProvider context

--- src/context/CartContext/CartProvider.jsx
+++ src/context/CartContext/CartProvider.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { CartContext } from "./CartContext";
 
-export const CartProvider = ({ children }) => {
+export function CartProvider({ children }) {
     const [cart, setCart] = useState([]);
 
     const exists = (id) => {
@@ -76,4 +76,4 @@ export const CartProvider = ({ children }) => {
     return (
         <CartContext.Provider value={values}>{children}</CartContext.Provider>
     );
-};
+}
