6af7b533813c608032aa2a731aee6cd9a4523d34
Mathias Ezequiel Latronico <mathiezelat@gmail.com>
2025-11-19 08:25:27 -0300

    Refactor ItemListContainer component

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,30 +2,16 @@ import { useEffect, useState } from "react";
 import { ItemList } from "../ItemList/ItemList";
 import { useParams } from "react-router-dom";
 import "./ItemListContainer.css";
+import { getProducts } from "../../services/products";
 
 export function ItemListContainer() {
     const [products, setProducts] = useState([]);
     const { category } = useParams();
 
     useEffect(() => {
-        fetch("https://690b8c336ad3beba00f56cc2.mockapi.io/products")
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error(
-                        "Ocurrió un error al obtener los productos"
-                    );
-                }
-
-                return response.json();
-            })
+        getProducts(category)
             .then((data) => {
-                if (category) {
-                    setProducts(
-                        data.filter((product) => product.category === category)
-                    );
-                } else {
-                    setProducts(data);
-                }
+                setProducts(data);
             })
             .catch((error) => {
                 console.error(error);
