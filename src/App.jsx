19b95a5de0f2a46b2e6822ab84f2ad1378b7b10f
Mathias Ezequiel Latronico <mathiezelat@gmail.com>
2025-11-19 08:25:15 -0300

    Refactor App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,45 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer";
 import { CartProvider } from "./context/CartContext/CartProvider";
 import { Cart } from "./components/Cart/Cart";
 import { ProductFormContainer } from "./components/adminComponents/ProductFormContainer/ProductFormContainer";
+import { MainLayout } from "./layouts/MainLayout";
+import { AdminLayout } from "./layouts/AdminLayout";
+import { ProtectedRoute } from "./components/ProtectedRoute/ProtectedRoute";
+import { Login } from "./components/Login/Login";
 import "./App.css";
 
 function App() {
     return (
         <BrowserRouter>
             <CartProvider>
-                <Header />
                 <Routes>
-                    <Route index element={<ItemListContainer />} />
-                    <Route path="/productos" element={<ItemListContainer />} />
-                    <Route
-                        path="/productos/:id"
-                        element={<ItemDetailContainer />}
-                    />
-                    <Route path="/carrito" element={<Cart />} />
-                    <Route path="/admin" element={<ProductFormContainer />} />
+                    <Route element={<MainLayout />}>
+                        <Route index element={<ItemListContainer />} />
+                        <Route
+                            path="/productos"
+                            element={<ItemListContainer />}
+                        />
+                        <Route
+                            path="/detalle/:id"
+                            element={<ItemDetailContainer />}
+                        />
+                        <Route path="/carrito" element={<Cart />} />
+                    </Route>
+
+                    <Route path="/admin" element={<AdminLayout />}>
+                        <Route index element={<Login />} />
+                        <Route
+                            path="alta-productos"
+                            element={
+                                <ProtectedRoute>
+                                    <ProductFormContainer />
+                                </ProtectedRoute>
+                            }
+                        />
+                    </Route>
                 </Routes>
                 <Footer />
             </CartProvider>
